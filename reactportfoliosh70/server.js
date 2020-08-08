ff67420c639784d3e261ae376240ca947dd8b625
shamlin143 <shamlin@gvtc.com>
2020-08-07 19:45:16 -0500

    Update server.js

--- reactportfoliosh70/server.js
+++ reactportfoliosh70/server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
