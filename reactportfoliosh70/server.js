43a0f34a8388d9eb6e0a0c33ae59c4874731536a
shamlin143 <shamlin@gvtc.com>
2020-08-07 20:13:22 -0500

    Update server.js

--- reactportfoliosh70/server.js
+++ reactportfoliosh70/server.js
@@ -9,7 +9,7 @@ const nodemailer = require('nodemailer');
 app.use(favicon(__dirname + 'reactportfoliosh70/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'reaactportfoliosh70')));
+app.use(express.static(path.join(__dirname, 'reactportfoliosh70')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
